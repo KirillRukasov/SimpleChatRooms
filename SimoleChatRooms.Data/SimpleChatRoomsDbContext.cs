36ced9bad044996d4515bc0abfcf46441278a60b
kirillRukasov <shadowsun67@gmail.com>
2023-10-07 17:16:44 -0300

    Add models, DB

---
+++ SimoleChatRooms.Data/SimpleChatRoomsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimpleChatRooms.Models;
+
+namespace SimpleChatRooms.Data
+{
+    public class SimpleChatRoomsDbContext : DbContext
+    {
+        public SimpleChatRoomsDbContext(DbContextOptions<SimpleChatRoomsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Chat>()
+                .HasKey(c => c.ChatId);
+
+            modelBuilder.Entity<Message>()
+                .HasKey(m => m.MessageId);
+
+            modelBuilder.Entity<Message>()
+                .HasOne(m => m.Chat)
+                .WithMany(c => c.Messages)
+                .HasForeignKey(m => m.ChatId);
+        }
+    }
+}
---
+++ SimpleChatRooms.Models/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChatRooms.Models
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public string Name { get; set; }
+        public int CreatorUserId { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
