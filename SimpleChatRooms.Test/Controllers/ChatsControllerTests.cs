42178f12f21320c555139fd52b9c872d18f8e7a2
kirillRukasov <shadowsun67@gmail.com>
2023-10-08 00:23:08 -0300

    add ChatsControllerTests, start ChatsController implementation

---
+++ SimpleChatRooms.Test/Controllers/ChatsControllerTests.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimpleChatRooms.Controllers;
+using SimpleChatRooms.Interfaces;
+using SimpleChatRooms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChatRooms.Test.Controllers
+{
+    public class ChatsControllerTests
+    {
+        private readonly Mock<IChatService> _mockChatService;
+        private readonly ChatsController _controller;
+
+        public ChatsControllerTests()
+        {
+            _mockChatService = new Mock<IChatService>();
+            _controller = new ChatsController(_mockChatService.Object);
+        }
+
+        [Fact]
+        public async Task Post_CreatesChatSuccessfully_ReturnsCreatedChat()
+        {
+            var testChat = TestData.GetTestChat();
+            _mockChatService.Setup(service => service.CreateChatAsync(testChat.Name, It.IsAny<int>()))
+                            .ReturnsAsync(testChat);
+
+            var result = await _controller.CreateChat(testChat.Name, testChat.CreatorUserId);
+
+            var actionResult = Assert.IsType<ActionResult<Chat>>(result);
+            var returnValue = Assert.IsType<Chat>(actionResult.Value);
+            Assert.Equal(testChat.Name, returnValue.Name);
+        }
+
+        [Fact]
+        public async Task Post_WithExistingChatName_ReturnsBadRequest()
+        {
+            var testChat = TestData.GetTestChat();
+            _mockChatService.Setup(service => service.CreateChatAsync(testChat.Name, It.IsAny<int>()))
+                            .ThrowsAsync(new InvalidOperationException("Chat with this name already exists."));
+
+            var result = await _controller.CreateChat(testChat.Name, testChat.CreatorUserId);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetChatByName_WithNonExistingName_ReturnsNotFound()
+        {
+            _mockChatService.Setup(service => service.GetChatByNameAsync(It.IsAny<string>()))
+                            .ReturnsAsync((Chat)null);
+
+            var result = await _controller.GetChatByName("NonExistingChat");
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task JoinChat_JoinsSuccessfully_ReturnsOk()
+        {
+            var chatName = "TestChat";
+            var userId = 1;
+
+            _mockChatService.Setup(service => service.JoinChatAsync(chatName, userId))
+                            .Returns(Task.CompletedTask);  
+
+            var result = await _controller.JoinChat(chatName, userId);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task JoinChat_NonExistingChat_ReturnsNotFound()
+        {
+            _mockChatService.Setup(service => service.JoinChatAsync(It.IsAny<string>(), It.IsAny<int>()))
+                            .ThrowsAsync(new InvalidOperationException("Chat does not exist."));
+
+            var result = await _controller.JoinChat("NonExistingChat", 1);
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task SendMessage_SendsSuccessfully_ReturnsOk()
+        {
+            var chatName = "TestChat";
+            var userId = 1;
+            var messageContent = "Hello, Chat!";
+
+            _mockChatService.Setup(service => service.SendMessageAsync(chatName, userId, messageContent))
+                            .Returns(Task.CompletedTask);
+
+            var result = await _controller.SendMessage(chatName, userId, messageContent);
+
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task SendMessage_EmptyMessage_ReturnsBadRequest()
+        {
+            _mockChatService.Setup(service => service.SendMessageAsync(It.IsAny<string>(), It.IsAny<int>(), string.Empty))
+                            .ThrowsAsync(new ArgumentException("Message cannot be empty."));
+
+            var result = await _controller.SendMessage("TestChat", 1, string.Empty);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task SendMessage_NonExistingChat_ReturnsNotFound()
+        {
+            _mockChatService.Setup(service => service.SendMessageAsync(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>()))
+                            .ThrowsAsync(new InvalidOperationException("Chat does not exist."));
+
+            var result = await _controller.SendMessage("NonExistingChat", 1, "Hello!");
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+    }
+}
---
+++ SimpleChatRooms/Controllers/ChatsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleChatRooms.Interfaces;
+using SimpleChatRooms.Models;
+
+namespace SimpleChatRooms.Controllers
+{
+    public class ChatsController : ControllerBase
+    {
+        private readonly IChatService _chatService;
+
+        public ChatsController(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
+        public async Task<ActionResult<Chat>> GetChatByName()
+        {
+
+        }
+
+        public async Task<ActionResult<Chat>> CreateChat()
+        {
+
+        }
+
+        public async Task<ActionResult> SendMessage()
+        {
+
+        }
+    }
+}
